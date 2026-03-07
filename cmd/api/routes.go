b4c88fd879e5f67101442fbb65cc99cc9d40c0d0
RUSIRUDEVINDA <rusirud49@gmail.com>
2026-03-07 23:03:24 +0530

    Add user registration and JWT-based authentication

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -13,13 +13,21 @@ func (app *application) routes() http.Handler {
 	// Logger = logs requests
 	// Recovery = catches panics and returns 500 instead of crashing
 
-	v1:= g.Group("/api/v1")
+	v1 := g.Group("/api/v1")
 	{
-	v1.GET("/events", app.getAllEvents)
-	v1.GET("/events/:id", app.getEvent)
-	v1.POST("/events", app.createEvent)
-	v1.PUT("/events/:id", app.updateEvent)
-	v1.DELETE("/events/:id", app.deleteEvent)
+		// Define routes for events
+		v1.GET("/events", app.getAllEvents)
+		v1.GET("/events/:id", app.getEvent)
+		v1.POST("/events", app.createEvent)
+		v1.PUT("/events/:id", app.updateEvent)
+		v1.DELETE("/events/:id", app.deleteEvent)
+
+		// Define routes for users
+		v1.POST("/auth/register", app.registerUser)
+
+		// Define attendance routes
+		v1.POST("/events/:id/attendees/:userId", app.addAttendeeToEvent)
+		v1.GET("/events/:id/attendees", app.getAttendeesForEvent)
 	}
 
 	return g
--- internal/database/users.go
+++ internal/database/users.go
@@ -1,6 +1,10 @@
 package database
 
-import "database/sql"
+import (
+	"context"
+	"database/sql"
+	"time"
+)
 
 // UserModel — Database connection hold කරනවා
 type UserModel struct {
@@ -15,4 +19,34 @@ type User struct {
 	Password string `json:"-"`     // json:"-" = NEVER send in response!
 	// Password ගෙ ඉදිරියේ "-" දෙව්වොත් JSON response
 	// එකේ password field show වෙන්නේ නෑ — Security!
+}
+
+func (m *UserModel) Insert(user *User) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	stmt := `INSERT INTO users (email, password, name) VALUES ($1, $2, $3) RETURNING id`
+	err := m.DB.QueryRowContext(ctx, stmt, user.Email, user.Password, user.Name).Scan(&user.Id)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// Add get user by id method
+func (m *UserModel) Get(id int) (*User, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	query := `SELECT * FROM users WHERE id = $1`
+
+	var user User
+	err := m.DB.QueryRowContext(ctx, query, id).Scan(&user.Id, &user.Email, &user.Name, &user.Password)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, nil
+		}
+		return nil, err
+	}
+	return &user, nil
 }
